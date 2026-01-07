91185b8d4d68cbf91e2b884b9135381cc92f79e5
dittmar <589530+GeorgeDittmar@users.noreply.github.com>
2026-01-06 21:44:23 -0800

    adding example script

---
+++ example.py
@@ -0,0 +1,13 @@
+from pydantask.agent import DeepAgent
+from pydantic import BaseModel
+from pydantic_ai.agent import Agent
+
+
+agent_registry = {}
+
+da = DeepAgent(
+    "I need help planning a trip to scotland. please provide a compiled trip plan itinerary."
+)
+result = da.run()
+
+print(result)
