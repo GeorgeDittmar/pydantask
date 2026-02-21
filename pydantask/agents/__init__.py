11319c6238bcd9e9d43ad2b3627a0c8e97c76d10
George Dittmar <georgedittmar@gmail.com>
2026-02-21 12:04:50 -0800

    Ahhhh (#9)
    * ahhhhh
    
    * hopefully fixed now

--- pydantask/agents/__init__.py
+++ pydantask/agents/__init__.py
@@ -1,4 +1,4 @@
-from pydantask.agents.agent import DeepAgent
-from pydantask.agents.spec import BaseAgentSpec, SupervisorSpec, ResearcherSpec
+from .agent import DeepAgent
+from .spec import BaseAgentSpec, SupervisorSpec, ResearcherSpec
 
 __all__ = ["DeepAgent", "BaseAgentSpec", "SupervisorSpec", "ResearcherSpec"]
