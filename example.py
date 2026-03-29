56c5f02a216fb85e3a287ab9ba62d813d8ba0163
George <georgedittmar@gmail.com>
2026-03-29 15:12:29 -0700

    updated example

--- example.py
+++ example.py
@@ -8,9 +8,10 @@ agent_registry = {}
 
 da = DeepAgent(
     # "Give me a detailed report on the history of the internet, including key milestones, influential figures, and its impact on society. The report should be well-structured with sections for each aspect of the topic, and include citations for all sources used in text and at the end.",
-    "Generate a detailed report on paranormal phenomena known as the ghost lights. Include historical accounts, scientific investigations, and cultural interpretations. The report should be well-structured with sections for each aspect of the topic, and include citations for all sources used in text and at the end.",
+    # "Generate a detailed report on paranormal phenomena known as the ghost lights. Include historical accounts, scientific investigations, and cultural interpretations. The report should be well-structured with sections for each aspect of the topic, and include citations for all sources used in text and at the end.",
     # "Give me an analysis of current deep agent frameworks and architectures in production. Each result or assertation must be cited clearly from where that information is from. This is to help validate the results, otherwise we cant trust the information.",
     # "I am traveling to London next month and want to know the best places to visit, eat, and stay. I also want to know about any events happening during that time. I would like a detailed itinerary for a 5 day trip that includes a mix of tourist attractions and local favorites. Please provide recommendations for each day, including activities, restaurants, and accommodations. Additionally, I would like to know about any cultural norms or tips for visiting London that I should be aware of.",
+    "Research what Turboquant is for me. I need a detailed report along with clear and simple explanations of what it is, how it works, and how to implement. Be sure to give me clear citations as well."
     model="gpt-5.1",
     trace=True,
 )
@@ -24,4 +25,6 @@ import json
 
 with open("output.json", "w") as json_file:
     json.dump(result.model_dump_json(), json_file, indent=4)
+
+
 pprint(result.final_result.summary)
